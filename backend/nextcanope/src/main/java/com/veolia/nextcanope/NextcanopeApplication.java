abd85c86e0b41f639f734105c8b74d4700594db2
Alexandre BERRIAU <alexandre.berriau.ext@veolia.com>
2023-02-02 12:30:04 +0100

    Ajout projet backend

---
+++ backend/nextcanope/src/main/java/com/veolia/nextcanope/NextcanopeApplication.java
@@ -0,0 +1,13 @@
+package com.veolia.nextcanope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextcanopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextcanopeApplication.class, args);
+	}
+
+}
---
+++ backend/nextcanope/src/test/java/com/veolia/nextcanope/NextcanopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veolia.nextcanope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextcanopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
