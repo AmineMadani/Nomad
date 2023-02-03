f60775e1fb49e7e17a3d5cce7211977416e621a2
Anthony YUNG <anthony.yung.ext@veolia.com>
2023-02-03 17:54:35 +0100

    Create drop_schemas.sql

---
+++ database/sql/drop_schemas.sql
@@ -0,0 +1,14 @@
+/*****************************
+ * Create extensions for APP *
+ *****************************/
+
+ create extension if not exists postgis;
+ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+/**************************
+ * drop schemas for APP *
+ **************************/
+
+drop schema if exists config cascade;
+drop schema if exists utils cascade;
+drop schema if exists settings cascade;
