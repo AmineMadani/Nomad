b7bcb12ea7ba4d30718773e8b56b918ab45f9e28
Alexandre BERRIAU <alexandre.berriau.ext@veolia.com>
2023-04-17 14:05:45 +0200

    Remove hash url

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -38,7 +38,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
