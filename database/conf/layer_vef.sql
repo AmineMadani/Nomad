34228b115bb854bb7fdd0428f8e81cf2534af53f
Anthony YUNG <anthony.yung.ext@veolia.com>
2023-05-12 14:43:22 +0200

    adapt config

--- database/conf/layer_vef.sql
+++ database/conf/layer_vef.sql
@@ -2,14 +2,13 @@
 set search_path to nomad, public;
 
 insert into layer_references(
-    lyr_table_name,
-    reference_key,
-    alias)
-select table_name::text, column_name, initcap(replace(column_name,'_',' '))
-  from information_schema.columns c
- where table_schema = 'asset'
-   and exists (select 1 from layer l where l.lyr_table_name::text =  table_name::text)
- order by 1, ordinal_position
+    lyr_id,
+    lrf_reference_key,
+    lrf_alias)
+    select l.id, column_name, initcap(replace(column_name,'_',' '))
+      from information_schema.columns c
+      join layer l on table_schema::text||'.'||table_name::text = l.lyr_table_name::text
+     order by 1, ordinal_position
     on conflict do nothing;
 
 -- get Alias from csv file
@@ -18,22 +17,23 @@ select table_name::text, column_name, initcap(replace(column_name,'_',' '))
 --   set pg_table = 'asset.' || pg_table;
 
 update layer_references r
-   set alias = t.alias_name
+   set lrf_alias = t.alias_name
   from public.temp_layer_references t
- where t.lyr_table_name::text = r.lyr_table_name::text
-   and t.column_name = r.reference_key;
+  join layer l on split_part(l.lyr_table_name::text,'.',2) = t.lyr_table_name::text
+ where t.column_name = r.lrf_reference_key;
 
 -- Insert default conf from csv
 
 insert into layer_references_default(
-    layer_reference_id,
-    position,
-    display_type,
-    section,
-    isvisible)
+    lrd_id,
+    lrd_position,
+    lrd_display_type,
+    lrd_section,
+    lrd_isvisible)
 select r.id, t.position::int, t.display_type::layer_references_display_type, t.section, case when t.notvisible = '1' then false else true end
   from layer_references r
-  join  public.temp_layer_references t on t.lyr_table_name::text = r.lyr_table_name::text and t.column_name = r.reference_key and t.display_type is not null
- order by r.lyr_table_name::text, 2::int;
+  join layer l on r.lyr_id = l.id
+  join public.temp_layer_references t on split_part(l.lyr_table_name::text,'.',2) = t.lyr_table_name::text and t.column_name = r.lrf_reference_key and t.display_type is not null
+ order by l.lyr_table_name::text, 2::int;
 
  DROP TABLE IF EXISTS public.temp_layer_references;
