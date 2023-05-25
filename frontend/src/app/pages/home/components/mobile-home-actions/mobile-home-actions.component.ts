dd41f203f620e2ac27bcce5bfe0656547cc0b5c5
Alexandre BERRIAU <alexandre.berriau.ext@veolia.com>
2023-05-25 09:31:04 +0200

    Generate workorder XY on mobile

--- frontend/src/app/pages/home/components/mobile-home-actions/mobile-home-actions.component.ts
+++ frontend/src/app/pages/home/components/mobile-home-actions/mobile-home-actions.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { firstValueFrom } from 'rxjs';
 import { DrawerRouteEnum } from 'src/app/core/models/drawer.model';
 import { DrawerService } from 'src/app/core/services/drawer.service';
+import { MapService } from 'src/app/core/services/map/map.service';
+import { ReferentialService } from 'src/app/core/services/referential.service';
 
 @Component({
   selector: 'app-mobile-home-actions',
@@ -9,7 +13,14 @@ import { DrawerService } from 'src/app/core/services/drawer.service';
   styleUrls: ['./mobile-home-actions.component.scss'],
 })
 export class MobileHomeActionsComponent implements OnInit {
-  constructor(private modalCtlr: ModalController, private drawerService: DrawerService) {}
+
+  constructor(
+    private modalCtlr: ModalController, 
+    private drawerService: DrawerService,
+    private mapService: MapService,
+    private referentialService: ReferentialService,
+    private router: Router
+  ) {}
 
   public type: 'DISPLAY' | 'ACTIONS' | 'TOOLS';
   public drawerRouteEnum = DrawerRouteEnum
@@ -19,5 +30,23 @@ export class MobileHomeActionsComponent implements OnInit {
   public navigateTo(location: DrawerRouteEnum): void {
     this.drawerService.navigateTo(location);
     this.modalCtlr.dismiss();
+  }
+
+  public async onGenerateWorkOrder(){
+    const centerMapPosition = this.mapService.getMap().getCenter();
+    let l_ctr_id = await firstValueFrom(this.referentialService.getReferentialIdByLongitudeLatitude('contract', centerMapPosition.lng.toString(), centerMapPosition.lat.toString()));
+    let l_cty_id = await firstValueFrom(this.referentialService.getReferentialIdByLongitudeLatitude('city', centerMapPosition.lng.toString(), centerMapPosition.lat.toString()));
+    let params:any = {};
+    params.x = centerMapPosition.lng;
+    params.y = centerMapPosition.lat;
+    params.lyr_table_name = "xy";
+    if(l_ctr_id && l_ctr_id.length > 0) params.ctr_id = l_ctr_id.join(',');
+    if(l_cty_id && l_cty_id.length > 0) params.cty_id = l_cty_id.join(',');
+
+    this.modalCtlr.dismiss();
+
+    this.router.navigate(['/home/work-order'], {
+      queryParams: params,
+    });
   }
 }
