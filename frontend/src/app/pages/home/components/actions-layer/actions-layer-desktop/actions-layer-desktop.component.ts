92746fc551fbb1b0d0d66cb70cdd0df9b5d27a71
Even SOULARD <even.soulard.ext@veolia.com>
2023-08-16 16:01:04 +0200

    Refactor: Improve print styling and code cleanliness
    - Remove unused imports.
    - Remove unused variables.
    - Implement @media print with a .no-print class set to display: none for a unified approach to hiding DOM elements during printing.
    - Within the @media print, directly specify classes of map-gl specific elements to hide.
    - Avoid route changes; the drawer can also be hidden during printing.
    - Remove added id tags which became redundant with the introduction of @media print.
    - Add comments to specific or less-obvious functions for clarity.

--- frontend/src/app/pages/home/components/actions-layer/actions-layer-desktop/actions-layer-desktop.component.ts
+++ frontend/src/app/pages/home/components/actions-layer/actions-layer-desktop/actions-layer-desktop.component.ts
@@ -1,20 +1,14 @@
 import {
   Component,
-  ElementRef,
   EventEmitter,
-  HostListener,
   Input,
   OnInit,
   Output,
   ViewChild,
 } from '@angular/core';
 import { DrawerRouteEnum } from 'src/app/core/models/drawer.model';
-import { MapComponent } from '../../map/map.component';
-import { IonPopover, ModalController } from '@ionic/angular';
+import { IonPopover } from '@ionic/angular';
 import { MapService } from 'src/app/core/services/map/map.service';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { NavigationEnd, Router } from '@angular/router';
-import { delay, filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-actions-layer-desktop',
@@ -22,7 +16,9 @@ import { delay, filter, take } from 'rxjs/operators';
   styleUrls: ['./actions-layer-desktop.component.scss'],
 })
 export class ActionsLayerDesktopComponent implements OnInit {
-  constructor(private mapService: MapService, private router: Router) {}
+  constructor(
+    private mapService: MapService
+  ) { }
 
   @ViewChild('toolbox', { static: true }) toolboxPopover: IonPopover;
 
@@ -32,29 +28,6 @@ export class ActionsLayerDesktopComponent implements OnInit {
 
   public drawerRouteEnum = DrawerRouteEnum;
   public isToolboxOpen: boolean = false;
-
-  private currentUri: string = null;
-  private mapHeightOrigine: string = null;
-
-  @HostListener('window:afterprint')
-  onafterprint() {
-    document.getElementById('actions-layer-desktop-content').style.visibility =
-      'visible';
-    document.getElementById('map-layers-fab').style.visibility = 'visible';
-    document.getElementById('map-right-buttons').style.visibility = 'visible';
-    document.getElementById('main-header').style.display = 'block';
-    document.getElementById('main-content-side-menu').style.visibility =
-      'visible';
-    (
-      document.getElementsByClassName(
-        'maplibregl-ctrl maplibregl-ctrl-scale'
-      )[0] as HTMLDivElement
-    ).style.visibility = 'visible';
-    if (this.currentUri) {
-      this.router.navigate([this.currentUri]);
-      this.currentUri = null;
-    }
-  }
 
   ngOnInit() {}
 
@@ -88,43 +61,10 @@ export class ActionsLayerDesktopComponent implements OnInit {
   }
 
   public async onClickDisplayPrintTool(): Promise<void> {
+    // Allows waiting for the action selection toolbox to be hidden
     this.toolboxPopover.dismiss();
     await this.toolboxPopover.onDidDismiss();
-    this.openPrint();
-  }
-
-  public async openPrint(): Promise<void> {
-    document.getElementById('actions-layer-desktop-content').style.visibility =
-      'hidden';
-    document.getElementById('map-layers-fab').style.visibility = 'hidden';
-    document.getElementById('map-right-buttons').style.visibility = 'hidden';
-    document.getElementById('main-header').style.display = 'none';
-    document.getElementById('main-content-side-menu').style.visibility =
-      'hidden';
-    (
-      document.getElementsByClassName(
-        'maplibregl-ctrl maplibregl-ctrl-scale'
-      )[0] as HTMLDivElement
-    ).style.visibility = 'hidden';
-
-    this.router.events
-      .pipe(
-        filter(
-          (event): event is NavigationEnd => event instanceof NavigationEnd
-        ),
-        take(1)
-      )
-      .subscribe((event: NavigationEnd) => {
-        setTimeout(() => {
-          window.print();
-        }, 1000);
-      });
-
-    if (!this.router.url.endsWith('/home')) {
-      this.currentUri = this.router.url;
-      this.router.navigate(['/home']);
-    } else {
-      window.print();
-    }
+    // Print
+    window.print();
   }
 }
