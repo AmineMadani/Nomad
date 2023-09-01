739749c179c47664ccc6d8fbeed412dc3b5ef012
Even SOULARD <even.soulard.ext@veolia.com>
2023-09-01 14:17:52 +0200

    Add organizational_units in db_installation.sh

--- database/bin/db_installation.sh
+++ database/bin/db_installation.sh
@@ -50,6 +50,7 @@ logInfo "========== Config of $ORGA ..."
 # apply custom scripts if any
 CONF_FILE=../conf/conf_$ORGA.sql
 LAYER_FILE=../conf/layer_$ORGA.sql
+ORGANIZATIONAL_UNIT_FILE=../conf/organizational_unit_$ORGA.sql
 #LAYER_WO_PARAMS_FILE=../conf/wo_mngt_params_$ORGA.sql
 echo $CONF_FILE
 
@@ -64,5 +65,8 @@ psql -v ON_ERROR_STOP=1 -v srid=$SRID $CONNINFO < $LAYER_FILE  >/dev/null || fai
 psql $CONNINFO -v ON_ERROR_STOP=1 -c "DROP TABLE if exists public.temp_layer_references; "
 # Delete temp table
 #psql $CONNINFO -v ON_ERROR_STOP=1 -c "DROP TABLE if exists public.temp_layer_references;" || logWarn "!! Impossible de construire table temp !!"
+psql $CONNINFO -v ON_ERROR_STOP=1 -c "DROP TABLE if exists public.orga_data_tmp; CREATE TABLE public.orga_data_tmp(region text, territoire text, identifiant_technique text, code_contrat text, code_comptable text, libelle_court_contrat text, libelle_long_contrat text, date_deliberation_signature text, date_de_visa_en_prefecture text, date_effet_du_contrat text, date_echeance_du_contrat text, date_fin_exploitation text, date_echeance_max text, date_fermeture_comptable text, libelle_insee text, code_insee text, date_entree_commune text, date_de_sortie_commune text, adhesion text, pourcent_couv_eau_ac text, pourcent_couv_anc text, code_societe text, libelle_societe text, code_activite text, nature_juridique text, mode_de_gestion text);" || logWarn "!! Impossible de construire table orga_data_tmp !!"
+psql $CONNINFO -v ON_ERROR_STOP=1 -c "\copy public.orga_data_tmp FROM '../conf/csv/rcc.csv' WITH DELIMITER ',' CSV" || logWarn "!! Impossible de charger csv !!"
+psql -v ON_ERROR_STOP=1 -v srid=$SRID $CONNINFO < $ORGANIZATIONAL_UNIT_FILE >/dev/null || failed "Impossible de charger les entités organisationnels de '$ORGANIZATIONAL_UNIT_FILE' !"
 
 logInfo "========== End of treatment !"
