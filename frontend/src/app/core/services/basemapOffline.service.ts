caaa2fb2b21a7fed84a745a82384e1f215402e71
Alexandre BERRIAU <alexandre.berriau.ext@veolia.com>
2023-09-15 19:01:22 +0200

    Offline basemap refactoring

--- frontend/src/app/core/services/basemapOffline.service.ts
+++ frontend/src/app/core/services/basemapOffline.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import { CapacitorSQLite, SQLiteConnection, SQLiteDBConnection, CapacitorSQLitePlugin } from '@capacitor-community/sqlite';
 import { Directory, Filesystem } from '@capacitor/filesystem';
+import * as pako from "pako";
+import MapLibreGL from "maplibre-gl";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
@@ -15,24 +18,42 @@ export class BasemapOfflineService {
     native: boolean = false;
     db: SQLiteDBConnection;
 
-    constructor() {
+    constructor(
+        private httpClient: HttpClient
+    ) {
     }
 
     /**
      * Plugin database Initialization
      */
     public async initializePlugin(): Promise<void> {
-        console.log("init plugin");
         this.platform = Capacitor.getPlatform();
         if (this.platform === 'ios' || this.platform === 'android') this.native = true;
-        if(this.native) {
+        if (this.native) {
             this.sqlitePlugin = CapacitorSQLite;
             this.sqliteConnection = new SQLiteConnection(this.sqlitePlugin);
             this.isService = true;
-            const dbList = await this.sqliteConnection.getDatabaseList();
-            if(dbList.values.includes('territorySQLite.db')){
-                this.openDatabase('territory', false, "no-encryption",1,false);
+            let dbList = [];
+            try {
+                dbList = (await this.sqliteConnection.getDatabaseList()).values;
+            } catch (e) {
+                dbList = [];
             }
+            if (dbList.includes('territorySQLite.db')) {
+                this.openDatabase('territory', false, "no-encryption", 1, false);
+            }
+            MapLibreGL.addProtocol("offline", (params, callback) => {
+                let coords = params.url.replace('offline://', '').replace('.vector.pbf', '').split("/");
+                this.getTileFromDatabase(Number(coords[0]), Number(coords[1]), Number(coords[2])).then((tileBuffer) => {
+                    if (tileBuffer) {
+                        callback(null, tileBuffer, null, null);
+                    } else {
+                        let message = `Tile is not in DB: ${params.url}`;
+                        callback(new Error(message));
+                    }
+                });
+                return { cancel: () => { } };
+            });
         }
     }
 
@@ -62,23 +83,72 @@ export class BasemapOfflineService {
      * Method to move the mbtile file to the database apk folder and rename extension in .db
      */
     public moveDatabasesAndAddSuffix(): Promise<void> {
-        return this.sqliteConnection.moveDatabasesAndAddSuffix('databaseCopy',['territory']);
+        return this.sqliteConnection.moveDatabasesAndAddSuffix('databaseCopy', ['territory']);
+    }
+
+    /**
+     * Method to delete a database
+     */
+    public async deleteDatabase(): Promise<void> {
+        await this.sqliteConnection.closeAllConnections();
+        CapacitorSQLite.deleteDatabase({ database: 'territory' });
     }
 
     /**
      * Download the offline basemap file
      */
-    public async downloadOfflineData(urlFile:string) {
+    public downloadOfflineData(urlFile: string) {
 
         Filesystem.addListener('progress', progress => {
             console.log(`Progress: ${((progress.bytes / progress.contentLength) * 100).toFixed(0)}%`);
         });
 
-        await Filesystem.downloadFile({
+        Filesystem.downloadFile({
             progress: true,
             directory: Directory.Data,
             path: 'territory',
             url: urlFile
+        }).then(async () => {
+            this.moveDatabasesAndAddSuffix().then(async () => {
+                let dbList = [];
+                try {
+                    dbList = (await this.sqliteConnection.getDatabaseList()).values;
+                } catch (e) {
+                    dbList = [];
+                }
+                if (dbList.includes('territorySQLite.db')) {
+                    this.openDatabase('territory', false, "no-encryption", 1, false);
+                }
+            });
         })
+    }
+
+    public async getTileFromDatabase(z: number, x: number, y: number): Promise<ArrayBuffer> {
+        let params = [z, x, Math.pow(2, z) - y - 1];
+        return new Promise<ArrayBuffer>(async (resolve, reject) => {
+            let tiles = await this.db.query("select HEX(tile_data) as tile_data_hex from tiles WHERE zoom_level = ? AND tile_column = ? AND tile_row = ? limit 1;", params);
+            if (tiles.values.length !== 1) {
+                reject(new Error("No tile..."));
+                return;
+            }
+            const hexData = tiles.values[0].tile_data_hex;
+            let binData = new Uint8Array(hexData.match(/.{1,2}/g).map((byte: string) => parseInt(byte, 16)));
+            let isGzipped = binData[0] === 0x1f && binData[1] === 0x8b;
+            if (isGzipped) {
+                binData = pako.inflate(binData);
+            }
+            resolve(binData);
+        });
+    }
+
+    public async getOfflineStyleLayer(): Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            this.httpClient.get("./assets/offline/offlineStyle.json", { observe: 'response' }).subscribe({
+                next: (response: any) => {
+                    resolve(response.body);
+                },
+                error: (err) => { reject(err) }
+            });
+        });
     }
 }
